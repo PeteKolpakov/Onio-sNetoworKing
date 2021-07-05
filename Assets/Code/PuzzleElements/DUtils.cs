39035b3c26172ec3e7f5f4b7ae9bd75101682018
Pete <pete.kolpakov@gmail.com>
2021-07-05 17:25:07 +0200

    Added new Functionalities For Switches Pink - Latch Blue - Momentary Green - Timed Momentary

---
+++ Assets/Code/PuzzleElements/DUtils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OniosNetworKing.Assets.Code
+{
+    public static class DUtils 
+    {
+        private const float SLIDE_SPEED = 0.2f;
+        private const float GROUND_OFFSET = 0.1f;
+
+        /// <summary>
+        /// Causes target gameobject to slide down untill it is 1/2 of its y scale below ground 
+        /// (if ground is 0)
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static IEnumerator SlideDown(GameObject target)
+        {
+            while (target.transform.position.y > -(target.transform.localScale.y / 2) + GROUND_OFFSET)
+            {
+                target.transform.position += new Vector3(0, -SLIDE_SPEED, 0);
+                yield return null;
+            }
+        }
+        public static IEnumerator SlideUpTo(GameObject target, Vector3 targetTransform)
+        {
+            while (target.transform.position.y < targetTransform.y)
+            {
+                target.transform.position += new Vector3(0, SLIDE_SPEED, 0);
+                yield return null;
+            }
+        }
+
+    }
+}
---
+++ Assets/Code/PuzzleElements/SwitchColor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OniosNetworKing.Assets.Code
+{
+    /// <summary>
+    /// Pink = Latch On Switch
+    /// Blue = Momentary Switch
+    /// Green = Timed Switch
+    /// </summary>
+    public enum SwitchColor
+    {
+        Pink = 0,
+        Blue,
+        Green
+    }
+}
