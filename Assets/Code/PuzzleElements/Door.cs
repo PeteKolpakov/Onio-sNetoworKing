427e3187212eac0a72e788935f5df71aaa654ee5
Pete <pete.kolpakov@gmail.com>
2021-07-09 18:56:16 +0200

    Finished playable level (Test Level 1)and got all the shit to work like a real game

--- Assets/Code/PuzzleElements/Door.cs
+++ Assets/Code/PuzzleElements/Door.cs
@@ -11,20 +11,42 @@ namespace Assets.Code
     {
         private Vector3 _initialPosition;
         private Collider _collider;
+        [SerializeField] private bool _startsOpen;
         private void Awake()
         {
             _collider = GetComponent<Collider>();
             _initialPosition = transform.position;
+            if (_startsOpen)
+            {
+                StartCoroutine(DUtils.SlideDown(gameObject));
+            }
         }
         public void Open()
         {
-            _collider.enabled = false;
-            StartCoroutine(DUtils.SlideDown(gameObject));
+            if (!_startsOpen)
+            {
+                _collider.enabled = false;
+                StartCoroutine(DUtils.SlideDown(gameObject));
+            }
+            else if (_startsOpen)
+            {
+                _collider.enabled = true;
+                StartCoroutine(DUtils.SlideUpTo(gameObject, _initialPosition, .2f));
+            }
         }
         public void Close()
         {
-            _collider.enabled = true;
-            StartCoroutine(DUtils.SlideUpTo(gameObject, _initialPosition, .2f));
+            if (!_startsOpen)
+            {
+                _collider.enabled = true;
+                StartCoroutine(DUtils.SlideUpTo(gameObject, _initialPosition, .2f));
+            }
+            else if (_startsOpen)
+            {
+                _collider.enabled = false;
+                StartCoroutine(DUtils.SlideDown(gameObject));
+            }
+
         }
     }
 }
