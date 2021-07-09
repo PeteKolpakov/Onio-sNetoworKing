c1ec3fdcc13a13acbcdd8ae7dbb024a0eec73364
Daniel Salas <salasdaniel1998@gmail.com>
2021-07-09 10:27:48 +0200

    Finished offline Chat, next is to hook up Photon Chat with the existing Photon settings in the Project

--- Assets/Code/Utilities/ChatManager.cs
+++ Assets/Code/Utilities/ChatManager.cs
@@ -8,31 +8,35 @@ namespace OniosNetworKing
 {
     public class ChatManager : MonoBehaviour
     {
-        [SerializeField] private int _maxMessageCount = 25;
+        [SerializeField] private string _username;
+        [SerializeField][Range(0,25)] private int _maxMessageCount = 25;
         [SerializeField] private List<Message> _messageList = new List<Message>();
         [SerializeField] private GameObject _chatPannel, _textObject;
         [SerializeField] private TMP_InputField _chatBox;
+        [SerializeField] private Color _playerMessage, _info;
         void Update()
         {
             if(_chatBox.text != "")
             {
                 if (Input.GetKeyDown(KeyCode.Return))
                 {
-                    SendMessageToChat(_chatBox.text);
+                    SendMessageToChat(_username+": "+_chatBox.text,Message.MessageType.PlayerMesage);
                     _chatBox.text = "";
                 }
+            }
+            else if(!_chatBox.isFocused && Input.GetKeyDown(KeyCode.Return))
+            {
+                _chatBox.ActivateInputField();
             }
             if (!_chatBox.isFocused)
             {
                 if (Input.GetKeyDown(KeyCode.Space))
                 {
-                    SendMessageToChat("You pressed the space key!");
-                    Debug.Log("Debug Filler yay");
+                    SendMessageToChat("You pressed the space key!", Message.MessageType.Info);
                 }
             }
-            Debug.Log(_chatBox.isFocused);
         }
-        public void SendMessageToChat(string message)
+        public void SendMessageToChat(string message, Message.MessageType messageType)
         {
             if (_messageList.Count >= _maxMessageCount)
             {
@@ -48,8 +52,25 @@ namespace OniosNetworKing
             newMessage.TextObject = newText.GetComponent<TextMeshProUGUI>();
 
             newMessage.TextObject.text = newMessage.Text;
+            newMessage.TextObject.color = MessageTypeColor(messageType);
 
             _messageList.Add(newMessage);
+        }
+        private Color MessageTypeColor(Message.MessageType messageType)
+        {
+            Color color = _info;
+            switch (messageType)
+            {
+                case Message.MessageType.PlayerMesage:
+                    color = _playerMessage;
+                    break;
+                case Message.MessageType.Info:
+                    color = _info;
+                    break;
+                default:
+                    break;
+            }
+            return color;
         }
     }
     [System.Serializable]
@@ -57,5 +78,12 @@ namespace OniosNetworKing
     {
         public string Text;
         public TextMeshProUGUI TextObject;
+        public MessageType Type;
+
+        public enum MessageType
+        {
+            PlayerMesage,
+            Info
+        }
     }
 }
