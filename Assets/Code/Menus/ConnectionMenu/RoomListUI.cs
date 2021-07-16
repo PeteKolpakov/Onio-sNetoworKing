784ff700b74d1711ff9f6f4a235e7a7ae0bc30d9
Pete <pete.kolpakov@gmail.com>
2021-07-16 08:24:27 +0200

    worked on making the menu

---
+++ Assets/Code/Menus/ConnectionMenu/RoomListUI.cs
@@ -0,0 +1,29 @@
+﻿using OniosNetworKing.Assets.Code.Menus.ConnectionMenu;
+using Photon.Pun;
+using Photon.Realtime;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OniosNetworKing.Assets.Code.ConnectionMenu
+{
+    class RoomListUI : MonoBehaviourPunCallbacks
+    {
+        [SerializeField]
+        private ConnectionModel _connectionModel;
+        [SerializeField]
+        private GameObject _roomListingPrefab;
+        [SerializeField]
+        private Transform _content;
+
+        public override void OnRoomListUpdate(List<RoomInfo> roomList)
+        {
+            foreach (var room in roomList)
+            {
+                RoomListing listing = Instantiate(_roomListingPrefab, _content).GetComponent<RoomListing>();
+                listing.SetText(room);
+            }
+        }
+    }
+}
---
+++ Assets/Code/Menus/ConnectionMenu/RoomListing.cs
@@ -0,0 +1,20 @@
+﻿using Photon.Realtime;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace OniosNetworKing.Assets.Code.Menus.ConnectionMenu
+{
+    class RoomListing : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI _text;
+        public void SetText(RoomInfo roomInfo)
+        {
+            _text.text = roomInfo.Name;
+        }
+
+    }
+}
