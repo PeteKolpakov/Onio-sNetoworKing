98bf9c94915a9f55bc544c5065b4bb3ed89bd24c
Pete <pete.kolpakov@gmail.com>
2021-07-16 12:31:46 +0200

    Added SetNickname functionality from connection menu

---
+++ Assets/Code/PlayerNameInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace OniosNetworKing.Assets.Code
+{
+    class PlayerNameInput : MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_InputField _nameInput;
+        private string _defaultName = "Player";
+        [SerializeField]
+        private ConnectionModel _connectionModel;
+
+        public void SetPlayerName()
+        {
+            if (_nameInput.text != "")
+            {
+                _connectionModel.RenameLocalPlayerTo(_nameInput.text);
+            }
+            else
+            {
+                _connectionModel.RenameLocalPlayerTo(_defaultName);
+            }
+        }
+
+    }
+}
